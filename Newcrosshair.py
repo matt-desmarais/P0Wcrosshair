726e5d9647d06d3b4660fd181ecfbd3e65e28cd4
Matt <matt.desmarais78@gmail.com>
2017-07-16 18:49:48 -0400

    fixed zooming functionality

--- Newcrosshair.py
+++ Newcrosshair.py
@@ -21,6 +21,48 @@ import smbus
 from BerryImu import BerryImu
 from squid import *
 
+
+globals = {
+    'zoom_step'     : 0.03,
+
+    'zoom_xy_min'   : 0.0,
+    'zoom_xy'       : 0.0,
+    'zoom_xy_max'   : 0.4,
+
+    'zoom_wh_min'   : 1.0,
+    'zoom_wh'       : 1.0,
+    'zoom_wh_max'   : 0.2
+}
+
+def update_zoom():
+    #print "Setting camera to (%s, %s, %s, %s)" % (globals['zoom_xy'], globals['zoom_xy'], globals['zoom_wh'], globals['zoom_wh'])
+    camera.zoom = (globals['zoom_xy'], globals['zoom_xy'], globals['zoom_wh'], globals['zoom_wh'])
+    print "Camera at (x, y, w, h) = ", camera.zoom
+
+def set_min_zoom():
+    globals['zoom_xy'] = globals['zoom_xy_min']
+    globals['zoom_wh'] = globals['zoom_wh_min']
+
+def set_max_zoom():
+    globals['zoom_xy'] = globals['zoom_xy_max']
+    globals['zoom_wh'] = globals['zoom_wh_max']
+
+def zoom_out():
+    if globals['zoom_xy'] - globals['zoom_step'] < globals['zoom_xy_min']:
+        set_min_zoom()
+    else:
+        globals['zoom_xy'] -= globals['zoom_step']
+        globals['zoom_wh'] += (globals['zoom_step'] * 2)
+    update_zoom()
+
+def zoom_in():
+    if globals['zoom_xy'] + globals['zoom_step'] > globals['zoom_xy_max']:
+        set_max_zoom()
+    else:
+        globals['zoom_xy'] += globals['zoom_step']
+        globals['zoom_wh'] -= (globals['zoom_step'] * 2)
+    update_zoom()
+
 def get_file_name():  # new
     return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
 
@@ -621,29 +663,18 @@ with picamera.PiCamera() as camera:
         # cycle through possible patterns:
         patternswitch(ovl,0)
         while True:
-
-                clkState = GPIO.input(clk)
+		clkState = GPIO.input(clk)
                 dtState = GPIO.input(dt)
                 if clkState != clkLastState:
                         if dtState != clkState:
-                                if counter <= 2:
-                                    counter += 1
-                                else:
-                                    counter = 3
+                                counter += 1
+				zoom_in()
                         else:
-                                if counter!=0:
-                                    counter -= 1
-                                else:
-                                    counter=0
-                        if counter==0:
-                            camera.zoom = (0, 0, 1.0, 1.0)
-                        if counter==1:
-                            camera.zoom = (0.25, 0.25, 0.5, 0.5)
-                        if counter==2:
-                            camera.zoom = (0.125, 0.125, 0.5, 0.5)
-                        if counter==3:
-                            camera.zoom = (0.0625, 0.0625, 0.5, 0.5)
+                                counter -= 1
+				zoom_out()
+                        print counter
                 clkLastState = clkState
+                sleep(0.01)
                 
         gyr_meas = imu.read_gyr_data()
         time.sleep(0.25)
