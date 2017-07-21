488ce787577ad154feacc8963102621c117f8820
Matt <matt.desmarais78@gmail.com>
2017-07-20 22:53:15 -0400

    updated pattern for vufine
    crosshair was too hard to see

--- include/patterns.py
+++ include/patterns.py
@@ -9,11 +9,11 @@ maxpat = 10
 
 # pattern1: Bruker style crosshair with circles and ticks
 def pattern1( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(0,y),(width,y),col,1)
-    cv2.line(arr,(x,0),(x,height),col,1)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
     i = 0
     for i in range(1, 8): 
-        cv2.circle(arr,(x,y),i*rad,col,1)
+        cv2.circle(arr,(x,y),i*rad,col,5)
         i += 1
     # ticks on the horizontal axis:
     intervalh = np.arange(0,width,float(rad)/10)
@@ -23,11 +23,11 @@ def pattern1( arr, width, height, x, y, rad, col ):
         diff = int(round(i))
         if j%5 == 0:    
             if not j%10 == 0:
-                cv2.line(arr,(x+diff,y-4),(x+diff,y+4),col,1)
-                cv2.line(arr,(x-diff,y-4),(x-diff,y+4),col,1)
+                cv2.line(arr,(x+diff,y-4),(x+diff,y+4),col,5)
+                cv2.line(arr,(x-diff,y-4),(x-diff,y+4),col,5)
         else:
-            cv2.line(arr,(x+diff,y-2),(x+diff,y+3),col,1)
-            cv2.line(arr,(x-diff,y-2),(x-diff,y+3),col,1)
+            cv2.line(arr,(x+diff,y-2),(x+diff,y+3),col,5)
+            cv2.line(arr,(x-diff,y-2),(x-diff,y+3),col,5)
         j += 1
     # ticks on the vertical axis:
     intervalv = np.arange(0,height,float(rad)/10)
@@ -37,22 +37,22 @@ def pattern1( arr, width, height, x, y, rad, col ):
         diff = int(round(k))
         if l%5 == 0:    
             if l%10 == 0:
-                cv2.line(arr,(x-6,y+diff),(x+6,y+diff),col,1)
-                cv2.line(arr,(x-6,y-diff),(x+6,y-diff),col,1)
+                cv2.line(arr,(x-6,y+diff),(x+6,y+diff),col,5)
+                cv2.line(arr,(x-6,y-diff),(x+6,y-diff),col,5)
             else:
-                cv2.line(arr,(x-4,y+diff),(x+4,y+diff),col,1)
-                cv2.line(arr,(x-4,y-diff),(x+4,y-diff),col,1)
+                cv2.line(arr,(x-4,y+diff),(x+4,y+diff),col,5)
+                cv2.line(arr,(x-4,y-diff),(x+4,y-diff),col,5)
         else:
-            cv2.line(arr,(x-2,y+diff),(x+2,y+diff),col,1)
-            cv2.line(arr,(x-2,y-diff),(x+2,y-diff),col,1)
+            cv2.line(arr,(x-2,y+diff),(x+2,y+diff),col,5)
+            cv2.line(arr,(x-2,y-diff),(x+2,y-diff),col,5)
         l += 1
     return    
 
 # pattern2: simple crosshair with ticks
 def pattern2( arr, width, height, x, y, rad, col ):
     # cv2.circle(arr,(x,y),rad,col,1)
-    cv2.line(arr,(0,y),(width,y),col,1)
-    cv2.line(arr,(x,0),(x,height),col,1)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
     # ticks on the horizontal axis:
     intervalh = np.arange(0,width,float(rad)/10)
     j = 0
@@ -61,14 +61,14 @@ def pattern2( arr, width, height, x, y, rad, col ):
         diff = int(round(i))
         if j%5 == 0:    
             if j%10 == 0:
-                cv2.line(arr,(x+diff,y-6),(x+diff,y+6),col,1)
-                cv2.line(arr,(x-diff,y-6),(x-diff,y+6),col,1)
+                cv2.line(arr,(x+diff,y-6),(x+diff,y+6),col,5)
+                cv2.line(arr,(x-diff,y-6),(x-diff,y+6),col,5)
             else:
-                cv2.line(arr,(x+diff,y-4),(x+diff,y+4),col,1)
-                cv2.line(arr,(x-diff,y-4),(x-diff,y+4),col,1)
+                cv2.line(arr,(x+diff,y-4),(x+diff,y+4),col,5)
+                cv2.line(arr,(x-diff,y-4),(x-diff,y+4),col,5)
         else:
-            cv2.line(arr,(x+diff,y-2),(x+diff,y+3),col,1)
-            cv2.line(arr,(x-diff,y-2),(x-diff,y+3),col,1)
+            cv2.line(arr,(x+diff,y-2),(x+diff,y+3),col,5)
+            cv2.line(arr,(x-diff,y-2),(x-diff,y+3),col,5)
         j += 1
     # ticks on the vertical axis:
     intervalv = np.arange(0,height,float(rad)/10)
@@ -78,79 +78,79 @@ def pattern2( arr, width, height, x, y, rad, col ):
         diff = int(round(k))
         if l%5 == 0:    
             if l%10 == 0:
-                cv2.line(arr,(x-6,y+diff),(x+6,y+diff),col,1)
-                cv2.line(arr,(x-6,y-diff),(x+6,y-diff),col,1)
+                cv2.line(arr,(x-6,y+diff),(x+6,y+diff),col,5)
+                cv2.line(arr,(x-6,y-diff),(x+6,y-diff),col,5)
             else:
-                cv2.line(arr,(x-4,y+diff),(x+4,y+diff),col,1)
-                cv2.line(arr,(x-4,y-diff),(x+4,y-diff),col,1)
+                cv2.line(arr,(x-4,y+diff),(x+4,y+diff),col,5)
+                cv2.line(arr,(x-4,y-diff),(x+4,y-diff),col,5)
         else:
-            cv2.line(arr,(x-2,y+diff),(x+2,y+diff),col,1)
-            cv2.line(arr,(x-2,y-diff),(x+2,y-diff),col,1)
+            cv2.line(arr,(x-2,y+diff),(x+2,y+diff),col,5)
+            cv2.line(arr,(x-2,y-diff),(x+2,y-diff),col,5)
         l += 1
     return    
 
 # pattern3: simple crosshair without ticks
 def pattern3( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(0,y),(width,y),col,1)
-    cv2.line(arr,(x,0),(x,height),col,1)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
     return    
 
 # pattern4: simple crosshair with circles (no ticks)
 def pattern4( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(0,y),(width,y),col,1)
-    cv2.line(arr,(x,0),(x,height),col,1)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
     i = 0
     for i in range(1, 8): 
-        cv2.circle(arr,(x,y),i*rad,col,1)
+        cv2.circle(arr,(x,y),i*rad,col,5)
         i += 1
     return    
 
 # pattern5: simple crosshair with one circle (no ticks)
 def pattern5( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(0,y),(width,y),col,2)
-    cv2.line(arr,(x,0),(x,height),col,2)
-    cv2.circle(arr,(x,y),rad,col,2)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
+    cv2.circle(arr,(x,y),rad,col,5)
     return    
 
 # pattern6: simple circle
 def pattern6( arr, width, height, x, y, rad, col ):
-    cv2.circle(arr,(x,y),rad,col,1)
+    cv2.circle(arr,(x,y),rad,col,5)
     return
 
 # pattern7: small center crosshair
 def pattern7( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(x-10,y),(x+10,y),col,1)
-    cv2.line(arr,(x,y-10),(x,y+10),col,1)
+    cv2.line(arr,(x-10,y),(x+10,y),col,5)
+    cv2.line(arr,(x,y-10),(x,y+10),col,5)
     return
 
 # pattern8: small center crosshair without center
 def pattern8( arr, width, height, x, y, rad, col ):
-    cv2.line(arr,(x-10,y),(x-3,y),col,1)
-    cv2.line(arr,(x,y-10),(x,y-3),col,1)
-    cv2.line(arr,(x+3,y),(x+10,y),col,1)
-    cv2.line(arr,(x,y+3),(x,y+10),col,1)
+    cv2.line(arr,(x-10,y),(x-3,y),col,5)
+    cv2.line(arr,(x,y-10),(x,y-3),col,5)
+    cv2.line(arr,(x+3,y),(x+10,y),col,5)
+    cv2.line(arr,(x,y+3),(x,y+10),col,5)
     return
 
 # pattern9: only a dot
 def pattern9( arr, width, height, x, y, rad, col ):
-    cv2.circle(arr,(x,y),2,col,-1)
+    cv2.circle(arr,(x,y),2,col,2)
     return
 
 # pattern10: grid
 def pattern10( arr, width, height, x, y, rad, col ):
     # center lines:
-    cv2.line(arr,(0,y),(width,y),col,1)
-    cv2.line(arr,(x,0),(x,height),col,1)
+    cv2.line(arr,(0,y),(width,y),col,5)
+    cv2.line(arr,(x,0),(x,height),col,5)
     i = rad
     j = rad
     # horizontal lines:
     while i < height:
-        cv2.line(arr,(0,y+i),(width,y+i),col,1)
-        cv2.line(arr,(0,y-i),(width,y-i),col,1)
+        cv2.line(arr,(0,y+i),(width,y+i),col,5)
+        cv2.line(arr,(0,y-i),(width,y-i),col,5)
         i += rad
     # vertical lines:
     while j < width:
-        cv2.line(arr,(x+j,0),(x+j,height),col,1)
-        cv2.line(arr,(x-j,0),(x-j,height),col,1)
+        cv2.line(arr,(x+j,0),(x+j,height),col,5)
+        cv2.line(arr,(x-j,0),(x-j,height),col,5)
         j += rad
     return
